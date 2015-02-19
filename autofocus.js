22904f490ec242bc2bebd88fce71879fd443f21a
Jon Hieb <hiebj@live.com>
2015-02-19 11:26:14 -0700

    fixed conflict problem with multiple autofocus directives in one controller

--- autofocus.js
+++ autofocus.js
@@ -5,26 +5,24 @@
         .directive('autofocus', Autofocus);
 
     function Autofocus($timeout) {
-        var dom,
-            delay;
-
         function link($scope, element, attrs) {
-            dom = element[0];
-            delay = attrs.autofocusDelay;
+            var dom = element[0],
+                delay = attrs.autofocusDelay;
+
             if (attrs.autofocus) {
                 focusIf($scope[attrs.autofocus]);
                 $scope.$watch(attrs.autofocus, focusIf);
             } else {
                 focusIf(true);
             }
-        }
 
-        function focusIf(condition) {
-            delay = delay || 0;
-            if (condition) {
-                $timeout(function() {
-                    dom.focus();
-                }, delay);
+            function focusIf(condition) {
+                delay = delay || 0;
+                if (condition) {
+                    $timeout(function() {
+                        dom.focus();
+                    }, delay);
+                }
             }
         }
 
